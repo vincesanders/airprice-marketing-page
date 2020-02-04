c45b6d373d3bcfc2831a6979f3fee15cb5dcbf20
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-02-04 10:12:55 -0800

    gone portable. get it?

--- js/scrollExample.js
+++ js/scrollExample.js
@@ -3,16 +3,19 @@ $(".owl-carousel").owlCarousel({
   loop: true,
   margin: 10,
   nav: true,
+  autoplay: true,
+  autoplayTimeout: 3500,
+  autoplayHoverPause: true,
 
   responsive: {
     0: {
       items: 1
     },
     500: {
-      items: 2
+      items: 3
     },
     800: {
-      items: 3
+      items: 5
     },
     1000: {
       items: 5
